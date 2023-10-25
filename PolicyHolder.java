ff22486744baaef5d969ac7224057ae947385171
masterHazzrd <warpd.hazzrd@gmail.com>
2023-10-25 18:55:09 -0400

    Update PolicyHolder.java
    Imported fields and methods from Policy class

---
+++ PolicyHolder.java
@@ -0,0 +1,133 @@
+public class PolicyHolder {
+
+    private int  policyHoldersAge;
+    private String policyHoldersFName, policyHolderLName, policyHoldersSmokingStatus;
+    private double policyHoldersHeight, policyHoldersWeight;
+
+    //default constructor
+    public PolicyHolder() {
+        
+        this.policyHoldersAge = 0;
+        this.policyHoldersFName = "";
+        this.policyHolderLName = "";
+        this.policyHoldersSmokingStatus = "";
+        this.policyHoldersHeight = 0.0;
+        this.policyHoldersWeight = 0.0;
+
+    }
+
+    //parameterized constructor
+    public PolicyHolder(String polHoldersFName, String polHoldersLName, int polHoldersAge, String polSmkStat, double polHoldersHeight, double polHoldersWeight) {
+        this.policyHoldersFName = polHoldersFName;
+        this.policyHolderLName = polHoldersLName;
+        this.policyHoldersAge = polHoldersAge;
+        this.policyHoldersSmokingStatus = polSmkStat;
+        this.policyHoldersHeight = polHoldersHeight;
+        this.policyHoldersWeight = polHoldersWeight;
+    }
+
+    /**
+     * The setPolHoldFName method set the policy holders first name.
+     * @param pHFName Sets the policy holders first name.
+     */
+    public void setPolHoldFName(String pHFName) {
+        this.policyHoldersFName = pHFName;
+    }
+
+    /**
+     * The getPolHoldFName returns the policy holders first name.
+     * @return Returns policy holders first name.
+     */
+    public String getPolHoldFName() {
+        return policyHoldersFName;
+    }
+
+    /**
+     * The pHLName method sets the policy holders last name
+     * @param pHLName Sets policy holders last name.
+     */
+    public void setPolHoldLName(String pHLName) {
+        this.policyHolderLName = pHLName;
+    }
+
+    /**
+     * The getPolHoldLName returns the policy holders last name.
+     * @return Returns policy holders last name.
+     */
+    public String getPolHoldLName() {
+        return policyHolderLName;
+    }
+
+    /**
+     * The setPolHoldAge sets the policy holders age.
+     * @param pHAge Sets ploicy holders age.
+     */
+    public void setPolHoldAge(int pHAge) {
+        this.policyHoldersAge = pHAge;
+    }
+
+    /**
+     * The getPolHoldAge returns policy holders age
+     * @return Returns policy holders age.
+     */
+    public int getPolHoldAge() {
+        return policyHoldersAge;
+    }
+
+    /**
+     * The pHSmkSt sets the policy holders smoking status.
+     * @param pHSmkSt Sets policy holders smoking status.
+     */
+    public void setPolHoldSmkStat(String pHSmkSt) {
+        this.policyHoldersSmokingStatus = pHSmkSt;
+    }
+
+    /**
+     * The getPolHoldSmkStat returns policy holders smoking status.
+     * @return Returns policy holders smoking status.
+     */
+    public String getPolHoldSmkStat() {
+        return policyHoldersSmokingStatus;
+    }
+
+    /**
+     * The setPolHoldHeight sets policy holders height.
+     * @param pHHeight Sets policy holders height.
+     */
+    public void setPolHoldHeight(double pHHeight) {
+        this.policyHoldersHeight = pHHeight;
+    }
+
+    /**
+     * The getPolHoldHeight returns policy holders height.
+     * @return Returns policy holders height.
+     */
+    public double getPolHoldHeight() {
+        return policyHoldersHeight;
+    }
+
+    /**
+     * The setPolHoldWeight sets the policy holders weight.
+     * @param pHWeight Sets policy holders weight.
+     */
+    public void setPolHoldWeight(double pHWeight) {
+        this.policyHoldersWeight = pHWeight;
+    }
+
+    /**
+     * The getPolHoldWeight returns policy holders weight.
+     * @return Returns policy holders weight.
+     */
+    public double getPolHoldWeight() {
+        return policyHoldersWeight;
+    }
+
+    /**
+     * The getPolHoldBMI returns policy holders BMI.
+     * @return Returns policy holders BMI.
+     */ 
+    public double getPolHoldBMI() {
+        return ((policyHoldersWeight * 703) / Math.pow(policyHoldersHeight, 2));
+    }
+
+}
